2116dd6a6f6beea5e1acfb1180c6b05c848a4b57
Henry Le <henryle10@gmail.com>
2023-09-22 19:57:38 -0700

    finished Billboard API CRUD

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -2,19 +2,27 @@
 
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { Billboard } from "@prisma/client";
 
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+    data: Billboard[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+    data
+}) => {
     const router = useRouter();
     const params = useParams();
+    
     return (
         <>
             <div className="flex items-center justify-between">
                 <Heading 
-                    title="Billboards (0)"
+                    title={`Billboard (${data.length})`}
                     description="Manage billboards for your store"
                 />
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
